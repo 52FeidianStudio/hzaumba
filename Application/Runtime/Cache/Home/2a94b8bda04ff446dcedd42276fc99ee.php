cf35f7dba6a797223c868892d58ff9327c8f682e
xbx <1060031655@qq.com>
2016-04-15 13:47:04 +0800

    huang

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -109,7 +109,7 @@ $(function(){
                     <input name="cs_anchor1" autocomplete="off" id="cs_pause1" type="radio" class="cs_anchor" >
                     <ul>
                         <div style="width: 100%; visibility: hidden; font-size: 0px; line-height: 0;">
-                            <a href=""><img src="/hzaumba/Public/image/<?php echo $info_image[0]['content']; ?>" style="width: 100%;" class="set-img"></a>
+                            <img src="/hzaumba/Public/image/<?php echo $info_image[0]['content']; ?>" style="width: 100%;" class="set-img">
                         </div>
                        		<?php $j=0; ?>
 						<?php if(is_array($info_image)): $i = 0; $__LIST__ = $info_image;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><li class="num<?php echo $j;$j++; ?> img">
--- Application/Runtime/Cache/Home/d79f3fe05d0fa0c5e478d3dfd1c523a5.php
+++ Application/Runtime/Cache/Home/d79f3fe05d0fa0c5e478d3dfd1c523a5.php
@@ -17,68 +17,72 @@
 
  -->
 <div class="top"></div>
-<div id="head">
-    <div class="logo">
-        <img src="/hzaumba/Public/front/images/mba_logo.png" alt="">
+<div id="top-wrapper">
+    <div id="head">
+        <div class="logo">
+            <img src="/hzaumba/Public/front/images/mba_logo.png" alt="">
+        </div>
+        <div class="head-word">
+            <p>理论与实务并重　本土与国际复合</p>
+        </div>
     </div>
-    <div class="head-word">
-        <p>理论与实务并重　本土与国际复合</p>
+    <div id="nav">
+        <ul class="first-ul">        
+            <li><a href="/hzaumba/index.php/Home/Index/index" class="first-a">首页</a></li>
+            <li>
+                <a href="javascript:;" class="first-a">中心概况</a>
+                <ul class="second-ul">
+                    <span></span>
+                    <li><a href="/hzaumba/index.php/Home/Center/center">中心简介</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Center/organization">组织机构</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Center/edu">教育理念</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;" class="first-a">项目介绍</a>
+                <ul class="second-ul">
+                    <span></span>
+                    <li><a href="/hzaumba/index.php/Home/Introduce/mbaprj">MBA项目</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Introduce/edpprj">EDP项目</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;" class="first-a">师资力量</a>
+                <ul class="second-ul">
+                    <span></span>
+                    <li><a href="/hzaumba/index.php/Home/Teacher/inschool">校内导师</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Teacher/outschool">校外导师</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Teacher/company">合作企业</a></li>
+                </ul>
+            </li>
+            <li><a href="/hzaumba/index.php/Home/Zhaosheng/zhaosheng" class="first-a">招生信息</a></li>
+            <li>
+                <a href="javascript:;" class="first-a">教学管理</a>
+                <ul class="second-ul">
+                    <span></span>
+                    <li><a href="/hzaumba/index.php/Home/Manage/peiyang">培养方案</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Manage/lunwen">论文指导</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Manage/shijian">管理实践</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Manage/course">课程安排</a></li>
+                    <li><a href="/hzaumba/index.php/Home/Manage/dabian">答辩安排</a></li>
+                </ul>
+            </li>
+            <li><a href="/hzaumba/index.php/Home/Career/career" class="first-a">职业发展</a></li>
+            <li>
+                <a href="javascript:;" class="first-a">MBA联合会</a>
+                <ul class="second-ul">
+                    <span></span>
+                    <li><a href="/hzaumba/index.php/Home/MbaUnion/zhangcheng">联合会章程</a></li>
+                    <li><a href="/hzaumba/index.php/Home/MbaUnion/zuzhi">组织机构</a></li>
+                    <li><a href="/hzaumba/index.php/Home/MbaUnion/schoolmate">校友风采</a></li>
+                </ul>
+            </li>
+            <li><a href="/hzaumba/index.php/Home/Contact/contact" class="first-a">联系我们</a></li>
+        </ul>
     </div>
 </div>
-<div id="nav">
-    <ul class="first-ul">        
-        <li><a href="/hzaumba/index.php/Home/Index/index" class="first-a">首页</a></li>
-        <li>
-            <a href="/hzaumba/index.php/Home/Center/center" class="first-a">中心概况</a>
-            <ul class="second-ul">
-                <span></span>
-                <li><a href="/hzaumba/index.php/Home/Center/center">中心简介</a></li>
-                <li><a href="/hzaumba/index.php/Home/Center/organization">组织机构</a></li>
-                <li><a href="/hzaumba/index.php/Home/Center/edu">教育理念</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="/hzaumba/index.php/Home/Introduce/mbaprj" class="first-a">项目介绍</a>
-            <ul class="second-ul">
-                <span></span>
-                <li><a href="/hzaumba/index.php/Home/Introduce/mbaprj">MBA项目</a></li>
-                <li><a href="/hzaumba/index.php/Home/Introduce/edpprj">EDP项目</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="/hzaumba/index.php/Home/Teacher/inschool" class="first-a">师资力量</a>
-            <ul class="second-ul">
-                <span></span>
-                <li><a href="/hzaumba/index.php/Home/Teacher/inschool">校内导师</a></li>
-                <li><a href="/hzaumba/index.php/Home/Teacher/outschool">校外导师</a></li>
-                <li><a href="/hzaumba/index.php/Home/Teacher/company">合作企业</a></li>
-            </ul>
-        </li>
-        <li><a href="/hzaumba/index.php/Home/Zhaosheng/zhaosheng" class="first-a">招生信息</a></li>
-        <li>
-            <a href="/hzaumba/index.php/Home/Manage/peiyang" class="first-a">教学管理</a>
-            <ul class="second-ul">
-                <span></span>
-                <li><a href="/hzaumba/index.php/Home/Manage/peiyang">培养方案</a></li>
-                <li><a href="/hzaumba/index.php/Home/Manage/lunwen">论文指导</a></li>
-                <li><a href="/hzaumba/index.php/Home/Manage/shijian">管理实践</a></li>
-                <li><a href="/hzaumba/index.php/Home/Manage/course">课程安排</a></li>
-                <li><a href="/hzaumba/index.php/Home/Manage/dabian">答辩安排</a></li>
-            </ul>
-        </li>
-        <li><a href="/hzaumba/index.php/Home/Career/career" class="first-a">职业发展</a></li>
-        <li>
-            <a href="/hzaumba/index.php/Home/MbaUnion/zhangcheng" class="first-a">MBA联合会</a>
-            <ul class="second-ul">
-                <span></span>
-                <li><a href="/hzaumba/index.php/Home/MbaUnion/zhangcheng">联合会章程</a></li>
-                <li><a href="/hzaumba/index.php/Home/MbaUnion/zuzhi">组织机构</a></li>
-                <li><a href="/hzaumba/index.php/Home/MbaUnion/schoolmate">校友风采</a></li>
-            </ul>
-        </li>
-        <li><a href="/hzaumba/index.php/Home/Contact/contact" class="first-a">联系我们</a></li>
-    </ul>
-</div>
+
+
 <script>
 $(function(){
     $(".first-ul li").mouseover(function(){
